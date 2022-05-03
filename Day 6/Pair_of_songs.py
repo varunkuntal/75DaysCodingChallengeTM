0a99f10a095a6deebcb1fa848a92e9d6570043ce
varunkuntal <varunkuntal@yahoo.in>
2022-05-03 09:31:38 +0530

    Game of Life - Day 42 of #100DaysOfCode - Optimized

--- Day 6/Pair_of_songs.py
+++ Day 6/Pair_of_songs.py
@@ -3,9 +3,29 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         
-        c = [0] * 60
-        res = 0
-        for t in time:
-            res += c[-t % 60]
-            c[t % 60] += 1
-        return res
+        # Optmized - TC: O(n), SC: O(1)
+        n = len(time)
+        ll = [0] * 60
+        count = 0
+                
+        for i in range(n):
+            mod = time[i] % 60
+            res = 60 - mod
+            
+            count += ll[res] if mod != 0 else ll[0]
+            
+            ll[mod] += 1            
+            
+        return count
+            
+            
+## Brute Force O(n^2), SC: O(1)
+#         n = len(time)
+#         count = 0
+#         for i in range(n):
+#             for j in range(i+1, n):
+#                 if (time[i] + time[j]) % 60 == 0:
+#                     count += 1
+                    
+                    
+#         return count
